94ac0acc4631bd749049709bb35a65a8609639bb
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-03-12 01:42:41 -0400

    Added the layout to the minification process

--- minify.sh
+++ minify.sh
@@ -1,3 +1,3 @@
 postcss src/css/animation.css > src/css/animation.min.css
 postcss src/css/style.css > src/css/style.min.css
-postcss src/css/style.css > src/css/style.min.css
+postcss src/css/layout.css > src/css/layout.min.css
