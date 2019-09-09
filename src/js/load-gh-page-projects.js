02b066c332219413361c72aeb2f3c1387ee0a731
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2019-09-09 11:02:22 -0400

    Started getting images and titles for projects from their read me's

--- src/js/load-gh-page-projects.js
+++ src/js/load-gh-page-projects.js
@@ -15,25 +15,33 @@ fetch("https://api.github.com/users/alexjamesmalcolm/repos?per_page=100")
   .then(repos => repos.filter(repo => !repo.fork))
   .then(repos =>
     repos.forEach(async repo => {
-      const imageUrl = `https://source.unsplash.com/random/?programming,code,${repo.name}`;
+      const imageUrl = `https://source.unsplash.com/featured/?programming,code,${repo.name}`;
       const projectsElement = document.querySelector(".projects");
-      const languagesResponse = await fetch(repo.languages_url);
-      const languages = await languagesResponse.json();
+      const languages = await fetch(repo.languages_url).then(response =>
+        response.json()
+      );
+      const readMe = await fetch(
+        `https://api.github.com/repos/alexjamesmalcolm/${repo.name}/readme`
+      )
+        .then(response => response.json())
+        .then(obj => obj.content)
+        .then(atob);
+      const readMeTitles = readMe.match(/(?<=# ).*/gm);
+      const readMeImages = readMe.match(/(?<=!\[.*\]\()[^ ]*/gm);
+      const title = readMeTitles ? readMeTitles[0] : repo.name;
       projectsElement.appendChild(
         htmlToElement(`<article class="project">
       <div class="front side">
-        <img alt="${repo.name}" src="${imageUrl}" />
+        <img alt="${title}" src="${
+          readMeImages ? readMeImages[0] : imageUrl
+        }" />
         <h3>
-          <a href="${repo.homepage}">
-            ${repo.name}
-          </a>
+          <a href="${repo.homepage}">${title}</a>
         </h3>
       </div>
       <div class="back side">
         <h3>
-          <a href="${repo.homepage}">
-            ${repo.name}
-          </a>
+          <a href="${repo.homepage}">${title}</a>
         </h3>
         <p>
           ${repo.description ? repo.description : ""}
